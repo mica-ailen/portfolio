141f72fcfeefc60743b0ed7a9a1856cb7eff8d37
Micaela <micaela.pawlowski22@gmail.com>
2022-05-30 02:43:17 +0100

    home page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import Logo from '../Sidebar/logo.png'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
 <div className='navbar'>
@@ -13,17 +14,22 @@ const Sidebar = () => (
         <NavLink exact="true" activeclassname="active" to="/">
             <FontAwesomeIcon icon={faHome} color="white"/>
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className="about" to="/about">
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
             <FontAwesomeIcon icon={faUser} color="white"/>
         </NavLink>
         <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
             <FontAwesomeIcon icon={faEnvelope} color="white"/>
-        </NavLink>
-    </nav>
-    <ul>
+            </NavLink>
+            </nav>
+            <ul className='icons'>
         <li>
             <a target="_blank" rel="noreferrer" href="#">
                 <FontAwesomeIcon icon={faLinkedin} color="white"/>
+            </a>
+            </li>
+            <li>
+            <a target="_blank" rel="noreferrer" href="#">
+                <FontAwesomeIcon icon={faGithub} color="white"/>
             </a>
             </li>
     </ul>
