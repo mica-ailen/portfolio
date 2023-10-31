6220af88f578a8f47fb9271b3d527d41c75dfa67
mica-ailen <101481046+mica-ailen@users.noreply.github.com>
2023-10-31 15:27:28 +0100

    Home done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
         </Route>
       </Routes>
